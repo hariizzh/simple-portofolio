7dc7f02f44a79f0254bbc3a7fca29e33d312764e
hariizzh <legendpalu12@gmail.com>
2024-10-08 03:25:40 +0700

    Menghapus beberapa condole.log

--- script.js
+++ script.js
@@ -8,11 +8,6 @@ window.onscroll = () => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        // console.log('top', top)
-        // console.log('offset top', offset)
-        // console.log('offset Height', height)
-        // console.log('offset total', height + offset)
-        // console.log('Before met:', id);
         if (top >= offset - height / 3) {
             let id = sec.getAttribute('id');
             // console.log('Condition met:', id);
